67ce6524e03c76896ee2dd77760232228ac4bd75
Popovich2005 <popich2005@mail.ru>
2024-08-21 15:17:06 +0300

    add loading view

--- GitHubFollowers/Manager/NetworkManager.swift
+++ GitHubFollowers/Manager/NetworkManager.swift
@@ -5,37 +5,37 @@
 //  Created by Алексей Попов on 07.08.2024.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    
-    let baseURL = "http://api.github.com/users/"
+    private let baseURL = "http://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endPoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endPoint) else {
-            completed(nil, "This username created an invalid request. Please try again.")
+            completed(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
             if let _ = error {
-                completed(nil, "Unable to complete request. Please check your internet connection.")
+                completed(.failure(.unableToComplete))
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Invalid response from the server. Please try again.")
+                completed(.failure(.invalidResponse))
                 return
             }
             
             guard let data = data else {
-                completed(nil, "The server responded with no data. Please try again.")
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -43,10 +43,10 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completed(.success(followers))
                 
             } catch {
-                completed(nil, "Failed to decode data. Please try again.")
+                completed(.failure(.invalidData))
             }
         }
         
---
+++ GitHubFollowers/Utillities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GitHubFollowers
+//
+//  Created by Алексей Попов on 07.08.2024.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The server responded with no data. Please try again."
+}
