36058e3dd9aa3fbbc117ab56dbcb1966f9dd4d18
Popovich2005 <popich2005@mail.ru>
2024-08-23 13:47:55 +0300

    create GFUserInfo

---
+++ GitHubFollowers/Utillities/Constans.swift
@@ -0,0 +1,12 @@
+//
+//  Constans.swift
+//  GitHubFollowers
+//
+//  Created by Алексей Попов on 23.08.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
