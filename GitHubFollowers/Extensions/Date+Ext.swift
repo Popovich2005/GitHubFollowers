60e404d745e04eb22cae534dbbbad73b5a61655a
Popovich2005 <popich2005@mail.ru>
2024-10-03 11:21:28 +0300

    Date_Conversions_Date_Label

---
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  GitHubFollowers
+//
+//  Created by Алексей Попов on 03.10.2024.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+}
