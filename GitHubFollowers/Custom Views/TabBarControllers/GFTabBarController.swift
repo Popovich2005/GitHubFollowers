69ca25e12d3a2a396c6356ad27b476c19efac4f4
Popovich2005 <popich2005@mail.ru>
2024-10-09 14:15:53 +0300

    UITabBarController_Screen_Constants

---
+++ GitHubFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  GFTabBarController.swift
+//  GitHubFollowers
+//
+//  Created by Алексей Попов on 09.10.2024.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesListNC()]
+        
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesListNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
